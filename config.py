7f006a1d4dd0927f9527e1afdb92d484144c21ba
Shivansh Soni <shivansh.007oni@gmail.com>
2025-09-14 20:13:48 +0530

    remove pycache

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///expenses.db'
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'expenses.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
